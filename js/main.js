e41e6f479bf82d365addd9724dce8fc2e5899ea3
toth8j <toth.jozsef8@mav-szk.hu>
2017-05-29 16:49:26 +0200

    HTML5_3 feltöltése 4. nap

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//Elemek kiválasztása.
+//@param selector: string, az elemet kiválasztó selector.
+function _q( selector ) {
+    var elements = document.querySelectorAll( selector );
+    return elements;
+}
