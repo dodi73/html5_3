3b7cbe03eda8aaec4937522e865715b0f5e86a65
toth8j <toth.jozsef8@mav-szk.hu>
2017-05-30 12:14:32 +0200

    HTML5_3 feltöltése 4. nap 2.video

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ function _q( selector ) {
     var elements = document.querySelectorAll( selector );
     return elements;
 }
+
+// Profil beviteli mező megjelenítése
+function showProfile() {
+    var div = document.querySelector(".profile-div");
+
+    if ( div.classList.contains("show") ) {
+        div.classList.remove("show");
+    } else {
+        div.classList.add("show");    
+    }
+}
